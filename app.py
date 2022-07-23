1b6b700081755fad405b94b0a4c6b15b613d4cd9
SAHEED5406 <syyed.saheed03@gmail.com>
2022-07-23 19:44:33 +0530

    added logging module

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from housing.logging import logging
 
 app = Flask(__name__)
 
 @app.route('/' ,methods = ['Get' ,'Post'])
 def index():
+    logging.info('we are testing logging module')
     return "Starting Machine learning project"
 
 if __name__ == "__main__":
