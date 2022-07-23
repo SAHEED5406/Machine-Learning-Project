fd4c1099bcba7da0e2069a560aa0f64a96fe47e3
SAHEED5406 <syyed.saheed03@gmail.com>
2022-07-23 20:26:34 +0530

    added exception module

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import sys
 from flask import Flask
 from housing.logging import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 @app.route('/' ,methods = ['Get' ,'Post'])
 def index():
-    logging.info('we are testing logging module')
+    try:
+        raise Exception("we are testing Exception module")
+    
+    except Exception as e :
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info('we are testing logging module')
     return "Starting Machine learning project"
 
 if __name__ == "__main__":
