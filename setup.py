4889c192b20f153ec5d9be696ca55be09c1e58c0
SAHEED5406 <syyed.saheed03@gmail.com>
2022-07-23 18:37:07 +0530

    added set up file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup ,find_packages
 from typing import List
 
 
 
 #Declaring variavles for setup functions
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "Saheed"
 REQUIREMENT_FILE_NAME = "requirements.txt"
 
@@ -17,14 +17,14 @@ def get_requirements_list():
     """
 
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 setup(
     name = PROJECT_NAME,
     version = VERSION,
     author = AUTHOR,
     description= "This is the housing predictor project",
-    packages = ["housing"],
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
 
