147168e0233a7f33fe8c03e8ece3f476f9db7c7f
SAHEED5406 <syyed.saheed03@gmail.com>
2022-07-31 23:56:59 +0530

    added data validation

--- demo.py
+++ demo.py
@@ -2,12 +2,17 @@ from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logging import logging
 from housing.config.configuration import Configuration
+from housing.component.data_validation import DataValidation
+from housing.component.data_ingestion import DataIngestion
 
 def main():
     try :
-        #pipeline = Pipeline()
-        #pipeline.run_pipeline()
-        Configuration().get_data_validation_config()
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+        #data_ingestion_config = Configuration().get_data_ingestion_config()
+        #data_ingestion_artifact = DataIngestion(data_ingestion_config).initiate_data_ingestion()
+        #data_validation_config = Configuration().get_data_validation_config()
+        #DataValidation(data_validation_config=data_validation_config ,data_ingestion_artifact=data_ingestion_artifact).initiate_data_validation()
     except Exception as e:
         logging.error(e)
 
