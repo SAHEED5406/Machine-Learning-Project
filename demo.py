354fd24224c98fe34acb154f6601ebf4be098d1d
SAHEED5406 <syyed.saheed03@gmail.com>
2022-08-01 22:39:51 +0530

    added data transformation config

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+from housing.component import data_transformation
 from housing.pipeline.pipeline import Pipeline
 from housing.exception import HousingException
 from housing.logging import logging
@@ -7,12 +8,13 @@ from housing.component.data_ingestion import DataIngestion
 
 def main():
     try :
-        pipeline = Pipeline()
-        pipeline.run_pipeline()
-        #data_ingestion_config = Configuration().get_data_ingestion_config()
+        #pipeline = Pipeline()
+        #pipeline.run_pipeline()
+        data_ingestion_config = Configuration().get_data_ingestion_config()
         #data_ingestion_artifact = DataIngestion(data_ingestion_config).initiate_data_ingestion()
-        #data_validation_config = Configuration().get_data_validation_config()
+        data_validation_config = Configuration().get_data_validation_config()
         #DataValidation(data_validation_config=data_validation_config ,data_ingestion_artifact=data_ingestion_artifact).initiate_data_validation()
+        data_transformation_config = Configuration().get_data_transformation_config()
     except Exception as e:
         logging.error(e)
 
